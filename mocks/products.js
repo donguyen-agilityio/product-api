e638c4c0cf9a51d52004a2bd4de9cfa048ddf4b5
Do Nguyen <do.nguyen@asnet.com.vn>
2024-08-01 16:53:07 +0700

    mock data

--- mocks/products.js
+++ mocks/products.js
@@ -3,12 +3,6 @@ const casual = require('casual');
 const { getRandomInt, getRandomIntInclusive } = require('../helpers');
 
 const types = ['Nike', 'Adidas', 'Vans', 'All Stars', 'Airmax'];
-const images = [
-    '/images/1.png',
-    '/images/2.png',
-    '/images/3.png',
-    '/images/4.png'
-];
 
 const createProduct = () => {
     const type = types[getRandomInt(types.length)];
@@ -18,10 +12,10 @@ const createProduct = () => {
         name: `Nike ${type} ${getRandomIntInclusive(0, 1000)}`,
         price: getRandomIntInclusive(5, 1000),
         image: [
-            images[getRandomInt(images.length)],
-            images[getRandomInt(images.length)],
-            images[getRandomInt(images.length)],
-            images[getRandomInt(images.length)]
+            'https://github.com/user-attachments/assets/fabd0a64-146d-4d8b-a4f1-ec2062cad620',
+            'https://github.com/user-attachments/assets/2279bb8e-c59a-499f-8b79-e9410c13e22d',
+            'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c',
+            'https://github.com/user-attachments/assets/ee481906-fc98-42eb-b715-1c33b3b6b3e4'
         ],
         colors: ['ff0000', '006CFF', '171717'],
         description: casual.description,
--- mocks/users.js
+++ mocks/users.js
@@ -4,25 +4,25 @@ const mockUsers = [
     {
         id: uuidv4(),
         name: 'Anh Nguyen',
-        avatar: '/images/anh.png',
+        avatar: 'https://github.com/user-attachments/assets/a6b92232-6256-4894-adb8-78db03904281',
         userName: 'anh.nguyen'
     },
     {
         id: uuidv4(),
         name: 'Minh Dinh',
-        avatar: '/images/minh.png',
+        avatar: 'https://github.com/user-attachments/assets/a6b92232-6256-4894-adb8-78db03904281',
         userName: 'minh.dinh'
     },
     {
         id: uuidv4(),
         name: 'Do Nguyen',
-        avatar: '/images/do.png',
+        avatar: 'https://github.com/user-attachments/assets/f3bf0119-8057-45e1-bb61-41be8e184905',
         userName: 'do.nguyen'
     },
     {
         id: uuidv4(),
         name: 'Ngan Thai',
-        avatar: '/images/ngan.png',
+        avatar: 'https://github.com/user-attachments/assets/a6b92232-6256-4894-adb8-78db03904281',
         userName: 'ngan.thai'
     }
 ];
