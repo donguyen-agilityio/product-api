9764e82bf02e83346b462f8c008b9eb1f51a68e6
Do Nguyen <do.nguyen@asnet.com.vn>
2024-08-06 15:58:21 +0700

    mock data banner image

--- mocks/settings.js
+++ mocks/settings.js
@@ -1,7 +1,7 @@
 const banner = {
   title: 'Adidas Men Running Sneakers',
   subTitle: 'Performance and design. Taken right to the edge.',
-  image: ''
+  image: 'https://github.com/user-attachments/assets/9d74358d-6e5c-49fa-b8aa-a6e323f12c7a'
 };
 
 module.exports = { banner };
